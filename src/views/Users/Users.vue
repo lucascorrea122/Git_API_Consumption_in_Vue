cb5dfe606a2b04de7ff9681040b8f4dce125ea65
LucasCorrea <eslucascorrea@gmail.com>
2021-01-17 01:50:14 -0300

    View Users List Users (incomplete)

--- src/views/Users/Users.vue
+++ src/views/Users/Users.vue
@@ -7,14 +7,20 @@
     <hr class="users__divider" />
     <div class="users__container">
       <input class="users-input" type="text" v-model="username" />
+      <button v-on:click="say({ username })">oi</button>
       <div class="users__listUsers" id="list-users"></div>
     </div>
-    <div id="app">
-      <ol>
-        <li v-for="repo in repos" :key="repo">{{ repo.name}}</li>
-      </ol>
+    <div class="users__group__container">
+      <div v-for="repo in repos" :key="repo" id="app">
+        <div class="users__cards">
+          <img class="users__image" :src="repo.avatar_url" alt="" />
+          <h1 class="user-name">{{ repo.login }}</h1>
+        </div>
+        <hr class="user-divider" />
+      </div>
     </div>
   </section>
+  <!-- v-for="repo in repos" :key="repo" -->
 </template>
 
 <script>
@@ -26,24 +32,28 @@ export default {
       titulo: "ola mundo",
       username: "",
       repos: [],
+      count: "",
     };
   },
-  watch: {
-    username: function () {
-      queryGithubAPI(this.username).then(repos=>{
-          if(repos.length > 0 && repos[0].id){
-              this.repos = repos
-          }
+  methods: {
+    say: function () {
+      queryGithubAPI(this.username).then((repos) => {
+        this.repos = repos.items;
       });
     },
   },
 };
 function queryGithubAPI(username) {
-  return fetch(`https://api.github.com/users/${username}/repos`, {
-    headers: {
-      authorization: "token b41b5136da9ca1e5a4e533d8066c1405dd2c276e"
+  const page = 1;
+  //   return fetch(`https://api.github.com/users/${username}/repos`, {
+  return fetch(
+    `https://api.github.com/search/users?q=${username}&page=${page}&per_page=10`,
+    {
+      headers: {
+        authorization: "token 9e4a4884107074449ed4ab4f5180f4f8d3234a39",
+      },
     }
-  }).then((response) => response.json());
+  ).then((response) => response.json());
 }
 </script>
 
